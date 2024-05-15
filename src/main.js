e321bccdbbc7b8ce2a6f40bba9dee89b5816b808
Den4ik382 <denlikhman1@gmail.com>
2024-05-15 10:15:12 +0200

    22

--- src/main.js
+++ src/main.js
@@ -2,5 +2,4 @@ import checkImage from './js/render-fuctions';
 
 const searchForm = document.querySelector('.form');
 const galleryEl = document.querySelector('.gallery');
-
 searchForm.addEventListener('submit', checkImage);
