dcfbf78413820ee45b86f93d22b74f7e94dbc6f6
Den4ik382 <denlikhman1@gmail.com>
2024-05-15 10:27:19 +0200

    33

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,19 @@
 const API_KEY = '43854532-e18365a3905b9485abf8e557c';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export const searchParams = wordSearch => {
-  const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: wordSearch,
+export default function getImages(searchWord) {
+  const searchList = new URLSearchParams({
+    key: `${API_KEY}`,
+    q: searchWord,
+    image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
   });
 
-  return fetch(`${BASE_URL}?${searchParams.toString()}`).then(response => {
+  return fetch(`${BASE_URL}?${searchList}`).then(response => {
     if (!response.ok) {
-      throw new Error();
+      throw new Error(response.status);
     }
-    console.log(response.json());
+    return response.json();
   });
-};
+}
